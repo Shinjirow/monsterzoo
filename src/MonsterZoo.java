06bac09eebebca4e7fb50c5b97a3a2c72b545872
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2020-10-10 05:56:53 +0900

    remove large methods

--- src/MonsterZoo.java
+++ src/MonsterZoo.java
@@ -19,25 +19,30 @@ public class MonsterZoo {
         // エンカウント処理
         int flg1 = (int) (Math.random() * 10);//0,1の場合はズーstation，7~9の場合はモンスター
         if (flg1 <= 1) {
-            System.out.println("ズーstationを見つけた！");
-            int b = (int) (Math.random() * 3);//ball,fruits,eggがランダムに出る
-            int f = (int) (Math.random() * 2);
-            int e = (int) (Math.random() * 2);
-            System.out.println("ボールを" + b + "個，" + "フルーツを" + f + "個" + "卵を" + e + "個Getした！");
-            aUser.addBalls(b);
-            aUser.addFruits(f);
-            if (e >= 1) {//卵を1つ以上Getしたら
-                //egg[]に10個以上卵がない場合は新しい卵データをセットする
-                aUser.addEggs(e);
-            }
+            this.encounterZooStation();
         } else if (flg1 >= 7) {
-            //monsterZukanからランダムにモンスターを出す
-            Monster m = Monsters.randomCall();
-            System.out.println(m + "が現れた！");
-
-            aUser.tryToCatch(m);
+            this.encounterMonster();
         }
+    }
 
+    private void encounterZooStation() {
+        System.out.println("ズーstationを見つけた！");
+        int b = (int) (Math.random() * 3);//ball,fruits,eggがランダムに出る
+        int f = (int) (Math.random() * 2);
+        int e = (int) (Math.random() * 2);
+        System.out.println("ボールを" + b + "個，" + "フルーツを" + f + "個" + "卵を" + e + "個Getした！");
+        aUser.addBalls(b);
+        aUser.addFruits(f);
+        if (e >= 1) //卵を1つ以上Getしたら
+            aUser.addEggs(e);
+    }
+
+    private void encounterMonster() {
+        //monsterZukanからランダムにモンスターを出す
+        Monster m = Monsters.randomCall();
+        System.out.println(m + "が現れた！");
+
+        aUser.tryToCatch(m);
     }
 
     private void hatch() {
--- src/User.java
+++ src/User.java
@@ -30,24 +30,33 @@ public class User {
 
     // バグっています　要修正
     public void tryToCatch(Monster m) {
-        IntStream.range(0, this.items.chanceOfThrow()).map(i -> {
-            int r = (int) (6 * Math.random());//0~5までの数字をランダムに返す
-            if (this.items.hasFruits()) {
-                System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
-                this.items.spendFruits();
-                r = r * 2;
+        for (int i = 0; i < this.items.chanceOfThrow(); i++) {
+            if (this.challenge(m)) {
+                break;
             }
-            return r;
-        }).forEach(r -> {
-            System.out.println(m + "にボールを投げた");
-            this.items.spendBalls();
-            if (m.canCatch(r)) {
-                System.out.println(m + "を捕まえた！");
-                this.monsters.addMonster(m);
-            } else {
-                System.out.println(m + "に逃げられた！");
-            }
-        });
+        }
+    }
+
+    private boolean challenge(Monster m) {
+        System.out.println(m + "にボールを投げた");
+        this.items.spendBalls();
+        if (m.canCatch(this.determineCaptureRate())) {
+            System.out.println(m + "を捕まえた！");
+            this.monsters.addMonster(m);
+            return true;
+        }
+        System.out.println(m + "に逃げられた！");
+        return false;
+    }
+
+    private int determineCaptureRate() {
+        int r = (int) (6 * Math.random());//0~5までの数字をランダムに返す
+        if (this.items.hasFruits()) {
+            System.out.println("フルーツを投げた！捕まえやすさが倍になる！");
+            this.items.spendFruits();
+            r = r * 2;
+        }
+        return r;
     }
 
     public void hatchEggs() {
